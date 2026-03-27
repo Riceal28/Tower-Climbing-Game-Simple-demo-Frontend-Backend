a6c55b3e3b30e34b4ce0201c640e0b3fa939a9b0
riceal <sunzhiming@codemao.cn>
2026-03-27 17:42:40 +0800

    feat: 角色功能相关基础支持

--- src/main/java/com/szm/demo/common/ApiConstant.java
+++ src/main/java/com/szm/demo/common/ApiConstant.java
@@ -2,4 +2,5 @@ package com.szm.demo.common;
 
 public interface ApiConstant {
     String API_USERS = "/api/user";
+    String API_PLAYER = "/api/player";
 }
--- src/main/java/com/szm/demo/common/RedisKeyConstants.java
+++ src/main/java/com/szm/demo/common/RedisKeyConstants.java
@@ -11,20 +11,26 @@ public enum RedisKeyConstants {
 //    MONSTER_INFO("monsterInfo:"),
 //    MONSTER_ACTION_INFO("monsterActionInfo:"),
 //    BATTLE_INFO("battleInfo:");
-    USER_INFO("demo:user:info:%s"),
-    USER_LOGIN("demo:user:login:%s"),
-    USER_DETAIL("demo:user:detail:%s"),
-    USER_TOKEN_IN("demo:user:token:%s"),
-    USER_TOKEN_OUT("demo:user:token:black:%s");
+    USER_INFO("demo:user:info:%s","userId"),
+    USER_DETAIL("demo:user:detail:%s","userId"),
+    USER_TOKEN_IN("demo:user:token:%s","JTI"),
+    USER_TOKEN_OUT("demo:user:token:blacklist:%s","JTI"),
+    LEVEL_INFO("demo:level:%s","level");
 
     private final String key;
+    private final String useBy;
 
-    RedisKeyConstants(String key) {
+    RedisKeyConstants(String key, String description) {
         this.key = key;
+        this.useBy = description;
     }
 
     public String getKey() {
         return this.key;
+    }
+
+    public String getUseBy() {
+        return useBy;
     }
 
     public String getKey(Object... args) {
