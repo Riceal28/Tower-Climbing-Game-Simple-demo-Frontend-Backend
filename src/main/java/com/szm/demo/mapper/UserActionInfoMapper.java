9236e19175cddf34be14aa3db474e7d9c31c466a
riceal <sunzhiming@codemao.cn>
2026-04-07 18:21:35 +0800

    add: 角色技能相关mapper

---
+++ src/main/java/com/szm/demo/mapper/UserActionInfoMapper.java
@@ -0,0 +1,29 @@
+package com.szm.demo.mapper;
+
+import com.szm.demo.entity.UserActionInfo;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface UserActionInfoMapper {
+
+    @Select("SELECT * FROM user_action_info WHERE user_id=#{userId}")
+    List<UserActionInfo> getByUserId(@Param("userId")Long userId);
+
+    @Options(useGeneratedKeys = true, keyProperty = "id", keyColumn = "id")
+    @Insert("INSERT INTO user_action_info (user_id, action_id, current_cd, rest_continue_round) " +
+            "VALUES (#{userActionInfo.userId},#{userActionInfo.actionId},#{userActionInfo.currentCd},#{userActionInfo.restContinueRound})")
+    void insert(@Param("userActionInfo")UserActionInfo userActionInfo);
+
+    @Insert({
+            "<script>",
+            "INSERT INTO user_action_info (user_id, action_id, current_cd, rest_continue_round) ",
+            "VALUES ",
+            "<foreach collection='userActionInfoList' item='item' separator=','>",
+            "(#{item.userId}, #{item.actionId}, #{item.currentCd}, #{item.restContinueRound})",
+            "</foreach>",
+            "</script>"
+    })
+    int batchInsert(@Param("userActionInfoList") List<UserActionInfo> userActionInfoList);
+}
