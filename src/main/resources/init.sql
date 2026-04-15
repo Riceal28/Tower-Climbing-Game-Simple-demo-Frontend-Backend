de08e9b4af37f92fffb46c8e0853542b8a21cd45
riceal <sunzhiming@codemao.cn>
2026-04-15 15:59:46 +0800

    fixed: 修复表结构无用字段

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -15,7 +15,7 @@ CREATE TABLE IF NOT EXISTS user_info
 -- 用户详情表, 用户展示详细信息(状态)
 CREATE TABLE IF NOT EXISTS user_palyer_info
 (
-    id              BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY COMMENT '主键ID',
+    id              BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY COMMENT '主键ID(角色ID)',
     user_id         BIGINT UNSIGNED NOT NULL COMMENT '关联用户ID',
     player_class    VARCHAR(20) NOT NULL DEFAULT 'SABER' COMMENT '职阶:SABER/ARCHER/CASTER',
     level           INT      NOT NULL DEFAULT 1 COMMENT '等级',
@@ -31,54 +31,55 @@ CREATE TABLE IF NOT EXISTS user_palyer_info
   DEFAULT CHARSET = utf8mb4
   COMMENT = '用户详情表';
 
--- 等级信息表,
+-- 等级信息表,--todo:不同职阶不同数值
 CREATE TABLE IF NOT EXISTS level_info
 (
-    id          INT UNSIGNED AUTO_INCREMENT PRIMARY KEY COMMENT '主键ID',
-    level       INT UNIQUE NOT NULL COMMENT '等级',
-    needed_exp  BIGINT     NOT NULL COMMENT '至下一级所需经验',
-    max_hp      INT        NOT NULL COMMENT '生命值上限',
-    max_mp      INT        NOT NULL COMMENT '法力值上限',
-    attack_base INT        NOT NULL COMMENT '基础攻击力',
-    create_time DATETIME   NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    update_time DATETIME   NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-    INDEX       idx_level (level) COMMENT '等级索引'
+    id              INT UNSIGNED AUTO_INCREMENT PRIMARY KEY COMMENT '主键ID',
+    player_class    VARCHAR(20) NOT NULL DEFAULT 'SABER' COMMENT '职阶:SABER/ARCHER/CASTER',
+    level           INT UNIQUE NOT NULL COMMENT '等级',
+    needed_exp      BIGINT     NOT NULL COMMENT '至下一级所需经验',
+    max_hp          INT        NOT NULL COMMENT '生命值上限',
+    max_mp          INT        NOT NULL COMMENT '法力值上限',
+    attack_base     INT        NOT NULL COMMENT '基础攻击力',
+    create_time     DATETIME   NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    update_time     DATETIME   NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    INDEX           idx_level (level) COMMENT '等级索引'
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
   COMMENT = '等级信息表';
 
--- 游戏进程表(非即时,仅存储战斗开始时的状态), 用于保存用户进度
-CREATE TABLE IF NOT EXISTS save_info--todo:调整技能组中的用户ID(根据需要变更为角色ID)
+-- 游戏存档表(仅存储战斗前状态), 用于保存用户进度
+CREATE TABLE IF NOT EXISTS save_info
 (
     id              BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY COMMENT '主键ID(存档ID)',
     user_id         BIGINT UNSIGNED NOT NULL DEFAULT 0 COMMENT '用户ID',
     player_id       BIGINT UNSIGNED NOT NULL COMMENT '角色ID',
     level           INT      NOT NULL DEFAULT 1 COMMENT '等级',
-    exp             INT      NOT NULL DEFAULT 0 COMMENT '经验',
+    exp             BIGINT   NOT NULL DEFAULT 0 COMMENT '经验',
     current_hp      INT      NOT NULL DEFAULT 6 COMMENT '当前生命值',
     current_mp      INT      NOT NULL DEFAULT 4 COMMENT '当前法力值',
 --     money        BIGINT UNSIGNED NOT NULL DEFAULT 0 COMMENT '金币',
     floor           INT UNSIGNED NOT NULL DEFAULT 1 COMMENT '所在层数',
     battle_order    INT UNSIGNED NOT NULL DEFAULT 0 COMMENT '当前遇敌顺序序号',
     progress        INT      NOT NULL DEFAULT 0 COMMENT '层数进度',
-    is_active       TINYINT  NOT NULL DEFAULT 0 COMMENT '激活状态(1激活或0未激活)',
+--     is_active       TINYINT  NOT NULL DEFAULT 0 COMMENT '激活状态(1激活或0未激活)',
     create_time     DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     update_time     DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
     INDEX           idx_user_id (user_id) COMMENT '用户ID索引'
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
-  COMMENT = '游戏进程表';
+  COMMENT = '游戏存档表';
 
 -- 战斗信息表, 展示单场战斗中的信息
 CREATE TABLE IF NOT EXISTS battle_info
 (
     id                     BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY COMMENT '主键ID(战斗ID)',
     save_id                BIGINT UNSIGNED NOT NULL COMMENT '存档ID',
-    user_id                BIGINT UNSIGNED NOT NULL COMMENT '用户ID',
+--     user_id                BIGINT UNSIGNED NOT NULL COMMENT '用户ID',
     monster_id             BIGINT UNSIGNED NOT NULL COMMENT '魔物ID',
-    user_current_hp        INT      NOT NULL DEFAULT 0 COMMENT '用户当前HP',
-    user_current_mp        INT      NOT NULL DEFAULT 0 COMMENT '用户当前MP',
-    user_current_defend    INT      NOT NULL DEFAULT 0 COMMENT '用户当前格挡值',
+    player_current_hp      INT      NOT NULL DEFAULT 0 COMMENT '角色当前HP',
+    player_current_mp      INT      NOT NULL DEFAULT 0 COMMENT '角色当前MP',
+    player_current_defend  INT      NOT NULL DEFAULT 0 COMMENT '角色当前格挡值',
     monster_current_hp     INT      NOT NULL DEFAULT 0 COMMENT '魔物当前HP',
     monster_current_mp     INT      NOT NULL DEFAULT 0 COMMENT '魔物当前MP',
     monster_current_defend INT      NOT NULL DEFAULT 0 COMMENT '魔物当前格挡值',
@@ -88,13 +89,13 @@ CREATE TABLE IF NOT EXISTS battle_info
   DEFAULT CHARSET = utf8mb4
   COMMENT = '战斗信息表';
 
--- 行为信息表(抽象化), 用于定义行为
+-- 技能信息表(抽象化), 用于定义技能
 CREATE TABLE IF NOT EXISTS action_info
 (
     id                 BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY COMMENT '主键ID',
-    action_id          BIGINT UNSIGNED NOT NULL COMMENT '行为ID',
+    action_id          BIGINT UNSIGNED NOT NULL COMMENT '技能ID',
     action_type        VARCHAR(20)  NOT NULL DEFAULT 'ATTACK' COMMENT '类型: ATTACK/DEFENSE/HEAL/SPELL/BUFF',
-    action_name        VARCHAR(50)  NOT NULL DEFAULT "" COMMENT '行为名',
+    action_name        VARCHAR(50)  NOT NULL DEFAULT "" COMMENT '技能名',
     description        VARCHAR(255) NOT NULL DEFAULT "" COMMENT '描述',
     target_is_for_self TINYINT      NOT NULL DEFAULT 0 COMMENT '作用对象是否自身,0对方,1自身',
 --     target_num         INT          NOT NULL DEFAULT 0 COMMENT '可作用对象数量(单体/AOE)',
@@ -104,20 +105,20 @@ CREATE TABLE IF NOT EXISTS action_info
     mp_cost            INT          NOT NULL DEFAULT 0 COMMENT 'MP消耗',
     is_continue        TINYINT      NOT NULL DEFAULT 0 COMMENT '是否持续性',
     continue_round     INT          NOT NULL DEFAULT 0 COMMENT '持续回合数',
-    cd                 INT          NOT NULL DEFAULT 0 COMMENT '行为冷却回合数',
+    cd                 INT          NOT NULL DEFAULT 0 COMMENT '技能冷却回合数',
     create_time        DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     update_time        DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间'
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
-  COMMENT = '行为信息表';
+  COMMENT = '技能信息表';
 
--- 用户行为关联表, 用于关联用户与行为
-CREATE TABLE IF NOT EXISTS user_action_info
+-- 角色技能关联表, 用于关联角色与技能
+CREATE TABLE IF NOT EXISTS player_action_info
 (
     id                  BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY COMMENT '主键ID',
     battle_id           BIGINT UNSIGNED NOT NULL COMMENT '对应的战斗ID',
-    user_id             BIGINT UNSIGNED NOT NULL COMMENT '用户ID',
-    action_id           BIGINT UNSIGNED NOT NULL COMMENT '行为ID',
+    player_id           BIGINT UNSIGNED NOT NULL COMMENT '角色ID',
+    action_id           BIGINT UNSIGNED NOT NULL COMMENT '技能ID',
     current_cd          INT      NOT NULL DEFAULT 0 COMMENT '该行为当前冷却时间',
     rest_continue_round INT      NOT NULL DEFAULT 0 COMMENT '剩余持续回合数',
     create_time         DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
@@ -125,9 +126,9 @@ CREATE TABLE IF NOT EXISTS user_action_info
     INDEX               idx_user_id (user_id) COMMENT '按创建时间查询索引'
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
-  COMMENT = '用户行为关联表';
+  COMMENT = '角色技能关联表';
 
--- 魔物信息表, 用于定义行为
+-- 魔物信息表, 用于定义魔物相关信息
 CREATE TABLE IF NOT EXISTS monster_info
 (
     id           BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY COMMENT '主键ID',
@@ -146,22 +147,22 @@ CREATE TABLE IF NOT EXISTS monster_info
   DEFAULT CHARSET = utf8mb4
   COMMENT = '魔物信息表';
 
--- 魔物行为关联表, 用于关联魔物的行为组
+-- 魔物技能关联表, 用于关联魔物的技能组
 CREATE TABLE IF NOT EXISTS monster_action_info
 (
     id                  BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY COMMENT '主键ID',
     battle_id           BIGINT UNSIGNED NOT NULL COMMENT '对应的战斗ID',
     monster_id          BIGINT UNSIGNED NOT NULL COMMENT '魔物ID',
-    action_id           BIGINT UNSIGNED NOT NULL COMMENT '行为ID',
+    action_id           BIGINT UNSIGNED NOT NULL COMMENT '技能ID',
     current_cd          INT      NOT NULL DEFAULT 0 COMMENT '该行为当前冷却时间',
     rest_continue_round INT      NOT NULL DEFAULT 0 COMMENT '剩余持续回合数',
     create_time         DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     update_time         DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间'
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
-  COMMENT = '魔物行为关联表';
+  COMMENT = '魔物技能关联表';
 
--- 塔信息表, 存储各层战斗信息
+-- 塔信息表, 存储各楼层基本信息
 CREATE TABLE IF NOT EXISTS tower_floor_info
 (
     id                  BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY COMMENT '主键ID',
@@ -173,18 +174,18 @@ CREATE TABLE IF NOT EXISTS tower_floor_info
     DEFAULT CHARSET = utf8mb4
     COMMENT = '魔塔-楼层配置表';
 
--- 层魔物关联表, 关联塔中每层的遇战信息
+-- 楼层魔物关联表, 关联塔中每层的战斗节点信息
 CREATE TABLE IF NOT EXISTS tower_floor_monster_info
 (
     id                  BIGINT  UNSIGNED AUTO_INCREMENT PRIMARY KEY COMMENT '主键ID',
     floor               INT     UNSIGNED NOT NULL COMMENT '层数',
-    monster_id          BIGINT  UNSIGNED NOT NULL COMMENT '魔物ID',
     battle_order        INT     UNSIGNED NOT NULL COMMENT '遇敌顺序',
-    -- todo:楼层进度奖励用作未来随机化功能
+    monster_id          BIGINT  UNSIGNED NOT NULL COMMENT '魔物ID',
+    -- todo:楼层进度奖励用作未来随机化遇敌功能
     reward_progress     INT      NOT NULL DEFAULT 0 COMMENT '击败后获得的层数进度(0-100)',
     create_time         DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     update_time         DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-    UNIQUE KEY          uk_floor_monster_order (floor, monster_id, battle_order) COMMENT '楼层+怪物+顺序唯一索引，防止重复配置'
+    UNIQUE KEY          uk_floor_monster_order (floor, battle_order, monster_id) COMMENT '楼层+怪物+顺序唯一索引，防止重复配置'
 ) ENGINE = InnoDB
     DEFAULT CHARSET = utf8mb4
     COMMENT = '魔塔-楼层魔物配置表';
