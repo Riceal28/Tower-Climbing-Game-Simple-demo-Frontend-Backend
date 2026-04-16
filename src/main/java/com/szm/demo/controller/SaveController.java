e88959a34eb05410890e046a6b3a80a8b84dcb1a
riceal <sunzhiming@codemao.cn>
2026-04-16 16:24:16 +0800

    remove: 移除无用的激活功能

--- src/main/java/com/szm/demo/controller/SaveController.java
+++ src/main/java/com/szm/demo/controller/SaveController.java
@@ -3,8 +3,8 @@ package com.szm.demo.controller;
 import com.szm.demo.common.ApiConstant;
 import com.szm.demo.common.Result;
 import com.szm.demo.entity.SaveInfo;
+import com.szm.demo.service.PlayerService;
 import com.szm.demo.service.SaveService;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,38 +17,38 @@ public class SaveController {
     @Autowired
     SaveService saveService;
 
+    @Autowired
+    PlayerService playerService;
+
+
     @PostMapping("/create")
-    public Result<String> createDefaultSave(HttpServletRequest request){
-        Long userId = Long.parseLong(request.getAttribute("userId").toString());
-        saveService.createDefaultSave(userId);
+    public Result<String> createDefaultSave(){
+        saveService.createDefaultSave();
         return Result.success("创建存档成功");
     }
 
     @GetMapping("/showall")
-    public Result<List<SaveInfo>> getSaves(HttpServletRequest request){
-        Long userId = Long.parseLong(request.getAttribute("userId").toString());
-        List<SaveInfo> saveInfoList = saveService.getSaveByUserId(userId);
+    public Result<List<SaveInfo>> getSaves(){
+        List<SaveInfo> saveInfoList = saveService.getSaveByUserId();
         return Result.success(saveInfoList);
     }
 
-    @GetMapping("/show/{id}")
-    public Result<SaveInfo> getOneSave(@PathVariable Long id ,HttpServletRequest request){
-        Long userId = Long.parseLong(request.getAttribute("userId").toString());
-        SaveInfo saveInfo = saveService.getSaveById(userId,id);
+    @GetMapping("/show")
+    public Result<SaveInfo> getOneSave(){
+        SaveInfo saveInfo = saveService.getSaveById();
         return Result.success(saveInfo);
     }
 
-    @PostMapping("/active/{id}")
-    public Result<String> playOneSave(@PathVariable Long id,HttpServletRequest request){
-        Long userId = Long.parseLong(request.getAttribute("userId").toString());
-        saveService.setSaveActive(userId,id);
-        return Result.success("激活存档成功");
+    @PostMapping("/save")
+    public Result<String> saveSave(@RequestBody SaveInfo saveInfo){
+        saveService.updateSave(saveInfo);
+        return Result.success("保存成功");
     }
 
-    @GetMapping("/showactive")
-    public Result<SaveInfo> getActiveSave(HttpServletRequest request){
-        Long userId = Long.parseLong(request.getAttribute("userId").toString());
-        SaveInfo saveInfo = saveService.getActiveSave(userId);
-        return Result.success(saveInfo);
+    @PostMapping("/load")
+    public Result<String> loadSave(@RequestBody SaveInfo saveInfo){
+        playerService.updatePlayerBySave(saveInfo);
+        return Result.success("加载成功");
     }
+
 }
